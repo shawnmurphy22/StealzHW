5c277559a69925224cf733f8bc13d6a37b2cd3b6
Shawn Murphy <spmurphy@ncsu.edu>
2014-06-17 14:35:59 -0400

    Pop-up window now displays Firmware, battery life, broadcasting strength, and distance from beacon.

--- src/com/example/stealzhomeworkshawn/MainActivity.java
+++ src/com/example/stealzhomeworkshawn/MainActivity.java
@@ -19,16 +19,13 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.RemoteException;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -38,9 +35,11 @@ import android.widget.TextView;
 import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
+import com.estimote.sdk.Utils;
+import com.estimote.sdk.connection.BeaconConnection;
 
 public class MainActivity extends ActionBarActivity {
-	
+
 	// Variables to be used
 	private LinearLayout myLinearLayout;
 	private TextView valueTV;
@@ -65,13 +64,14 @@ public class MainActivity extends ActionBarActivity {
 	private static final String WEBSITE = "website";
 	private static final String IMAGE = "image";
 	private static final String IMAGE_ROW = "image_row";
-	
+
 	//beacon stuff
 	private static final String ESTIMOTE_PROXIMITY_UUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
 	private static final Region ALL_ESTIMOTE_BEACONS = new Region("regionId", ESTIMOTE_PROXIMITY_UUID, null, null);
 	private BeaconManager beaconManager = new BeaconManager(this);
 	private Beacon beaconTemp;
-	
+	private BeaconConnection connection;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -114,7 +114,7 @@ public class MainActivity extends ActionBarActivity {
 			}
 			// get the layout so we cna build it dynamically
 			myLinearLayout = (LinearLayout) findViewById(R.id.linearLayout1);
-			
+
 			//add LayoutParams
 			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 			// set the orientation
@@ -134,11 +134,11 @@ public class MainActivity extends ActionBarActivity {
 			valueIm = new ImageView(this);
 			new DownloadImage(valueIm).execute(image);
 			valueIm.setLayoutParams(params);
-			
+
 			valueIm2 = new ImageView(this);
 			new DownloadImage(valueIm2).execute(imageRow);
 			valueIm2.setLayoutParams(params);
-			 //build the popUp layout
+			//build the popUp layout
 			layoutPopUp = new LinearLayout(this);
 			layoutPopUp.setOrientation(LinearLayout.VERTICAL);
 			// create popUp
@@ -153,7 +153,7 @@ public class MainActivity extends ActionBarActivity {
 			btn.setText("OK");
 			//specify the click action
 			btn.setOnClickListener(new OnClickListener() {
-				
+
 				@Override
 				public void onClick(View v) {
 					// TODO Auto-generated method stub
@@ -171,8 +171,8 @@ public class MainActivity extends ActionBarActivity {
 			layoutPopUp.addView(btn, params);
 			// add layout to popUp
 			popUp.setContentView(layoutPopUp);
-			
-			
+
+
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
@@ -183,117 +183,107 @@ public class MainActivity extends ActionBarActivity {
 		// beacon manager
 		beaconManager.setRangingListener(new BeaconManager.RangingListener() {
 			// Discovery function
-@Override public void onBeaconsDiscovered(Region region, final List<Beacon> beacons) {
-		// check for beacons
-	runOnUiThread(new Runnable() {
-          @Override
-          public void run() {
-        	  	if((beacons.size() >= 1) && (count < 1)) {
-			// get the closest beacon
-			beaconTemp = beacons.get(0);
-			// display the closest beacons name
-			valueTV2.setText(beaconTemp.getName() + " is in range!"  );
-    		// position the popUp window in middle of screen
-			popUp.showAtLocation(myLinearLayout, Gravity.CENTER, 0, 0);
-            popUp.update(Gravity.CENTER_HORIZONTAL, Gravity.CENTER_VERTICAL, 300, 200);
-            // increment count
-            count++;
-        // check for when beacon is out of range    
-		}else if( (beacons.size() == 0) && ( count > 0 )) {
-			// Update and show pop-up window
-			valueTV2.setText("No beacons in range!");
-			popUp.showAtLocation(myLinearLayout, Gravity.CENTER, 0, 0);
-            popUp.update(Gravity.CENTER_HORIZONTAL, Gravity.CENTER_VERTICAL, 300, 200);
-			count--;
-		
-		}
-          }
-	});
-}
-});
+			@Override public void onBeaconsDiscovered(Region region, final List<Beacon> beacons) {
+				// Make sure we don't run on main UI thread
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						// check for beacons
+						if((beacons.size() >= 1) && (count < 1)) {
+							// get the closest beacon
+							beaconTemp = beacons.get(0);
+							// connect to the closest beacon
+							connection = new BeaconConnection(getApplicationContext(), beaconTemp, createConnectionCallback());
+							connection.authenticate();
+							// increment count
+							count++;
+							// check for when beacon is out of range    
+						}else if( (beacons.size() == 0) && ( count > 0 )) {
+							// Update and show pop-up window
+							valueTV2.setText("Beacon out of range!");
+							popUp.showAtLocation(myLinearLayout, Gravity.CENTER, 0, 0);
+							popUp.update(Gravity.CENTER_HORIZONTAL, Gravity.CENTER_VERTICAL, 200, 150);
+							connection.close();
+							count--;
+
+						}
+					}
+				});
+			}
+		});
 	}
-	
+
+
 	@Override
 	protected void onStart() {
 		super.onStart();
 		beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
-		    @Override public void onServiceReady() {
-		      try {
-		        beaconManager.startRanging(ALL_ESTIMOTE_BEACONS);
-		      } catch (RemoteException e) {
-		        Log.e("TAG", "Cannot start ranging", e);
-		      }
-		    }
-		  });
+			@Override public void onServiceReady() {
+				try {
+					beaconManager.startRanging(ALL_ESTIMOTE_BEACONS);
+				} catch (RemoteException e) {
+					Log.e("TAG", "Cannot start ranging", e);
+				}
+			}
+		});
 	}
-	
+
 	@Override
-	public void onStop() {
+	protected void onStop() {
 		super.onStop();
 		try {
-		    beaconManager.stopRanging(ALL_ESTIMOTE_BEACONS);
-		  } catch (RemoteException e) {
-		    Log.e("TAG", "Cannot stop but it does not matter now", e);
-		  }
+			beaconManager.stopRanging(ALL_ESTIMOTE_BEACONS);
+		} catch (RemoteException e) {
+			Log.e("TAG", "Cannot stop but it does not matter now", e);
+		}
 
-		  // When no longer needed. Should be invoked in #onDestroy.
-		  beaconManager.disconnect();
+		// When no longer needed. Should be invoked in #onDestroy.
+		beaconManager.disconnect();
 	}
-	
+
+	@Override
+	protected void onDestroy()  {
+		connection.close();
+		super.onDestroy();
+	}
+
+
 	@TargetApi(Build.VERSION_CODES.CUPCAKE)
 	private class DownloadImage extends AsyncTask<String, Void, Bitmap> {
-	    ImageView bmImage;
+		ImageView bmImage;
 
-	    public DownloadImage(ImageView bmImage) {
-	        this.bmImage = bmImage;
-	    }
+		public DownloadImage(ImageView bmImage) {
+			this.bmImage = bmImage;
+		}
 
-	    protected Bitmap doInBackground(String... urls) {
-	        String url = urls[0];
-	    	// open up an http connection
-	        Bitmap mapTemp = null;
-	        try {
-	        	URL urlTemp = new URL(url);
-		        HttpURLConnection connection = (HttpURLConnection) urlTemp.openConnection();
-		        connection.setDoInput(true);
-		        connection.connect();
-		        InputStream input = connection.getInputStream();
-	            mapTemp = BitmapFactory.decodeStream(input);
-	        } catch (Exception e) {
-	            Log.e("Error", e.getMessage());
-	            e.printStackTrace();
-	        }
-	        return mapTemp;
-	    }
+		protected Bitmap doInBackground(String... urls) {
+			String url = urls[0];
+			// open up an http connection
+			Bitmap mapTemp = null;
+			try {
+				URL urlTemp = new URL(url);
+				HttpURLConnection connection = (HttpURLConnection) urlTemp.openConnection();
+				connection.setDoInput(true);
+				connection.connect();
+				InputStream input = connection.getInputStream();
+				mapTemp = BitmapFactory.decodeStream(input);
+			} catch (Exception e) {
+				Log.e("Error", e.getMessage());
+				e.printStackTrace();
+			}
+			return mapTemp;
+		}
 
-	    protected void onPostExecute(Bitmap result) {
-	        bmImage.setImageBitmap(result);
-	    }
+		protected void onPostExecute(Bitmap result) {
+			bmImage.setImageBitmap(result);
+		}
 	}
-	// Used to get the bitmap of a image from a URL
-//	public static Bitmap getBitmapFromURL(String src) {
-//	    try {
-//	    	// open up an http connection
-//	        URL url = new URL(src);
-//	        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//	        connection.setDoInput(true);
-//	        connection.connect();
-//	        InputStream input = connection.getInputStream();
-//	        // decode the image to be a bitmap
-//	        Bitmap myBitmap = BitmapFactory.decodeStream(input);
-//	        return myBitmap;
-//	    } catch (IOException e) {
-//	        e.printStackTrace();
-//	        Log.e("Exception",e.getMessage());
-//	        return null;
-//	    }
-//	}
-	
+
 	// Used to read in the JSON feed
 	public String JSONData() {
 		String jsonString = null;
 		try {
-			// Open and read the jsonfeed file
+			// Open and read the JSONfeed file
 			InputStream is = this.getResources().openRawResource(R.raw.jsonfeed);
 			int size = is.available();
 			byte[] buffer = new byte[size];
@@ -301,7 +291,7 @@ public class MainActivity extends ActionBarActivity {
 			is.close();
 			// add the feed to the string
 			jsonString = new String(buffer, "UTF-8");
-			
+
 		} catch (IOException ex) {
 			ex.printStackTrace();
 			return null;
@@ -330,21 +320,37 @@ public class MainActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
+	// Used when connecting to the beacon 
+	private BeaconConnection.ConnectionCallback createConnectionCallback() {
+		return new BeaconConnection.ConnectionCallback() {
+			// Connecting to the beacon
+			@Override public void onAuthenticated(final BeaconConnection.BeaconCharacteristics beaconChars) {
+				// Make sure we don't run on main UI thread
+				runOnUiThread(new Runnable() {
+					@Override public void run() {
+						// gather the information and put it together
+						StringBuilder sb = new StringBuilder()
+						.append("Firmware: ").append(beaconChars.getSoftwareVersion()).append("\n")
+						.append("Broadcasting power: ").append(beaconChars.getBroadcastingPower()).append(" dBm\n")
+						.append("Battery: ").append(beaconChars.getBatteryPercent()).append(" %\n")
+						.append("Distance: ").append(String.format("%.2f",Utils.computeAccuracy(beaconTemp))).append(" m");
+						// add the string to the textview
+						valueTV2.setText(sb.toString());
+						// position the popUp window in middle of screen
+						popUp.showAtLocation(myLinearLayout, Gravity.CENTER, 0, 0);
+						popUp.update(Gravity.CENTER_HORIZONTAL, Gravity.CENTER_VERTICAL, 200, 150);
+					}
+				});
+			}
 
-		public PlaceholderFragment() {
-		}
+			@Override public void onAuthenticationError() {
 
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
+			}
+
+			@Override public void onDisconnected() {
+				
+			}
+		};
 	}
 
 }
